849694d293322e1d02df8d557d8d286e26badaa6
conorbailey90 <conbailey90@gmail.com>
2020-11-15 22:10:56 +0000

    Quote form added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let welcomeSection = document.querySelector('.welcome');
 let quote = document.querySelector('.quote');
 let heroImage = document.querySelector('.hero-img');
 let heroText = document.querySelector('.hero-text-p');
+let about = document.querySelector('.about-container');
 
 window.addEventListener('DOMContentLoaded', reveal);
+window.addEventListener('scroll', scrollReveal)
 
 function reveal(){
     heroImage.classList.add('active')
@@ -15,4 +17,17 @@ function reveal(){
         },300) 
     },500)
     
+}
+
+function scrollReveal(e){
+    console.log(window.scrollY)
+
+    let distanceFromTop = -100
+    let aboutTop = about.getBoundingClientRect().top;
+  
+    if((aboutTop - window.innerHeight) < distanceFromTop){
+        about.classList.add('active')
+    }else{
+        about.classList.remove('active')
+    }
 }
